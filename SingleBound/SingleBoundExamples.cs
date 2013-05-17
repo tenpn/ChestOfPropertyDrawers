46fa520a86ce750a1f6c95cfb701b49994101a51
Andrew Fray <andrew@spryfox.com>
2013-05-17 22:32:19 +0100

    added NotLessThan examples

--- SingleBound/SingleBoundExamples.cs
+++ SingleBound/SingleBoundExamples.cs
@@ -8,4 +8,10 @@ public class SingleBoundExamples : MonoBehaviour
 
     [NotMoreThan(10)]
     public int NotMoreThan10;
+
+    [NotLessThan(4f)]
+    public float NotLessThan4f = 5f;
+
+    [NotLessThan(-3)]
+    public int NotLessThanMinus3;
 }
